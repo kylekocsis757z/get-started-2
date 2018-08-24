5bacbc6e7a571d32db23931be0804a9e1672c3b9
kylekocsis757z <42642284+kylekocsis757z@users.noreply.github.com>
2018-08-24 11:26:07 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <?php
-header("Location: https://www.amazon.com/gp/product/B000L8EEPS/ref=s9u_ri_gw_i4?ie=UTF8&fpl=fresh&pd_rd_i=B000L8EEPS&pd_rd_r=3e8e126b-7f8b-11e8-bacd-f54ec4cb866e&pd_rd_w=GSKau&pd_rd_wg=9Xy9x&pf_rd_m=ATVPDKIKX0DER&pf_rd_s=&pf_rd_r=YB4TK66VQWKTKMQHW88R&pf_rd_t=36701&pf_rd_p=7d5cf278-349e-40f5-bf15-6262ea74cc90&pf_rd_i=desktop
-");
+header("Location: https://www.thetablecolumbus.com");
 die();
 ?>
