4f0decf9c2e25150e1d30de3ec544c3600991c7a
kylekocsis757z <42642284+kylekocsis757z@users.noreply.github.com>
2018-09-01 19:53:02 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
-header("Location: https://www.amazon.com/gp/product/B075LZVCZV/ref=s9_acsd_top_hd_bw_b2YwNuF_c_x_w?pf_rd_m=ATVPDKIKX0DER&pf_rd_s=merchandised-search-3&pf_rd_r=2CT4GM0RXPJJEHMVM8QM&pf_rd_t=101&pf_rd_p=9fc62bd6-49c9-53e1-a8a9-ce531d333b9b&pf_rd_i=2348576011");
+header("Location: https://autorepairbartlesville.com");
 die();
 ?>
